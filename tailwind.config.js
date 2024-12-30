a8312b6727df20248e4c21ecb97637e53d88e1e2
Payam Dowlatyari <pdowlatyari@gmail.com>
2024-12-29 17:42:12 -0800

    feat: style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 
-import svgToDataUri from "mini-svg-data-uri";
-import colors from "tailwindcss/colors";
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+const svgToDataUri = require("mini-svg-data-uri");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 function addVariablesForColors({ addBase, theme }) {
   const allColors = flattenColorPalette(theme("colors"));
@@ -14,162 +16,166 @@ function addVariablesForColors({ addBase, theme }) {
   });
 }
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{js,ts,jsx,tsx,mdx}"];
-export const theme = {
-  extend: {
-    animation: {
-      'shimmer': "shimmer 2s linear infinite",
-      'text-shimmer': 'text-shimmer 2.5s ease-out infinite alternate',
-      'spotlight': "spotlight 2s ease .75s 1 forwards",
-      "border-width": "border-width 3s infinite alternate",
-      "background-shine": "background-shine 2s linear infinite",
-      first: "moveVertical 30s ease infinite",
-      second: "moveInCircle 20s reverse infinite",
-      third: "moveInCircle 40s linear infinite",
-      fourth: "moveHorizontal 40s ease infinite",
-      fifth: "moveInCircle 20s ease infinite",
-      marquee: 'marquee var(--duration, 30s) linear infinite',
-      "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
-      shine: "shine var(--duration) infinite linear",
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      animation: {
+        'shimmer': "shimmer 2s linear infinite",
+        'text-shimmer': 'text-shimmer 2.5s ease-out infinite alternate',
+        'spotlight': "spotlight 2s ease .75s 1 forwards",
+        "border-width": "border-width 3s infinite alternate",
+        "background-shine": "background-shine 2s linear infinite",
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        marquee: 'marquee var(--duration, 30s) linear infinite',
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        shine: "shine var(--duration) infinite linear",
+      },
+      keyframes: {
+        'shimmer': {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+        'text-shimmer': {
+          from: { backgroundPosition: '0 0' },
+          to: { backgroundPosition: '-200% 0' },
+        },
+        'spotlight': {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        "border-width": {
+          "from": {
+            "width": "10px",
+            "opacity": "0"
+          },
+          "to": {
+            "width": "100px",
+            "opacity": "1"
+          }
+        },
+        "background-shine": {
+          "from": {
+            "backgroundPosition": "0 0"
+          },
+          "to": {
+            "backgroundPosition": "-200% 0"
+          }
+        },
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+        marquee: {
+          to: { transform: 'translateX(-50%)' }
+        },
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
+        shine: {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+      },
     },
-    keyframes: {
-      'shimmer': {
-        from: {
-          backgroundPosition: "0 0",
-        },
-        to: {
-          backgroundPosition: "-200% 0",
-        },
-      },
-      'text-shimmer': {
-        from: { backgroundPosition: '0 0' },
-        to: { backgroundPosition: '-200% 0' },
-      },
-      'spotlight': {
-        "0%": {
-          opacity: 0,
-          transform: "translate(-72%, -62%) scale(0.5)",
-        },
-        "100%": {
-          opacity: 1,
-          transform: "translate(-50%,-40%) scale(1)",
-        },
-      },
-      "border-width": {
-        "from": {
-          "width": "10px",
-          "opacity": "0"
-        },
-        "to": {
-          "width": "100px",
-          "opacity": "1"
-        }
-      },
-      "background-shine": {
-        "from": {
-          "backgroundPosition": "0 0"
-        },
-        "to": {
-          "backgroundPosition": "-200% 0"
-        }
-      },
-      moveHorizontal: {
-        "0%": {
-          transform: "translateX(-50%) translateY(-10%)",
-        },
-        "50%": {
-          transform: "translateX(50%) translateY(10%)",
-        },
-        "100%": {
-          transform: "translateX(-50%) translateY(-10%)",
-        },
-      },
-      moveInCircle: {
-        "0%": {
-          transform: "rotate(0deg)",
-        },
-        "50%": {
-          transform: "rotate(180deg)",
-        },
-        "100%": {
-          transform: "rotate(360deg)",
-        },
-      },
-      moveVertical: {
-        "0%": {
-          transform: "translateY(-50%)",
-        },
-        "50%": {
-          transform: "translateY(50%)",
-        },
-        "100%": {
-          transform: "translateY(-50%)",
-        },
-      },
-      marquee: {
-        to: { transform: 'translateX(-50%)' }
-      },
-      "border-beam": {
-        "100%": {
-          "offset-distance": "100%",
-        },
-      },
-      shine: {
-        "0%": {
-          "background-position": "0% 0%",
-        },
-        "50%": {
-          "background-position": "100% 100%",
-        },
-        to: {
-          "background-position": "0% 0%",
-        },
-      },
+    colors: {
+      black: "#141414",
+      "ash-black": "#222424",
+      white: "#f4f4f2",
+      "ultra-light-gray": "#9c9c9c",
+      apple: "#e1e1e1",
+      green: "#00ff00",
+      red: "#ff0000",
+      yellow: "#ffd500",
+      silver: "#d2d4d6",
+      "white-smoke": "#d8d8d8",
+      ...colors,
     },
   },
-  colors: {
-    black: "#141414",
-    "ash-black": "#222424",
-    white: "#f4f4f2",
-    "ultra-light-gray": "#9c9c9c",
-    apple: "#e1e1e1",
-    green: "#00ff00",
-    red: "#ff0000",
-    yellow: "#ffd500",
-    silver: "#d2d4d6",
-    "white-smoke": "#d8d8d8",
-    ...colors,
-  },
+
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+          "bg-dot-thick": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+          
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
-export const plugins = [
-  addVariablesForColors,
-  function ({ matchUtilities, theme }) {
-    matchUtilities(
-      {
-        "bg-grid": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-grid-small": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-dot": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-          )}")`,
-        }),
-        "bg-dot-thick": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-          )}")`,
-        }),
-      },
-      { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-    );
-  },
-];
 
 
